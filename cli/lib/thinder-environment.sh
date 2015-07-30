0e8958d166c659cc2d4c012562a19e39fa8ebcaa
Bas du Pré <me@basdupre.com>
2015-07-31 01:45:26 +0200

    images start

--- cli/lib/thinder-environment.sh
+++ cli/lib/thinder-environment.sh
@@ -12,3 +12,28 @@ function thinder_check_storage_environment {
 		return 1
 	fi
 }
+
+function is_writable {
+	if touch "$1/.__tmp_writecheck" &> /dev/null; then
+		rm -f "$1/.__tmp_writecheck"
+		return 0
+	else
+		return 1
+	fi
+}
+
+function thinder_check_storage {
+	thinder_check_storage_environment || return 1
+	if ! mount | grep -q "$THINDER_ROOT"; then
+		critical "No storage container has been connected to the Thinder environment"
+		notice "expected storage environment mountpoint: '$THINDER_ROOT'"
+		info "use 'thinder storage connect' to connect a storage container"
+		return 1
+	fi
+	if ! is_writable "$THINDER_ROOT"; then
+		critical "The storage container is not writable"
+		notice "The storage container at '$THINDER_ROOT' is not writable by the current user ($(whoami))"
+		return 1
+	fi
+	return 0
+}
