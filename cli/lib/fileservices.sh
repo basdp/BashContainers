b73d9040e9694bfbf55fc2ade594519560eec38b
Bas du Pré <me@basdupre.com>
2015-08-06 09:38:55 +0200

    TYPO

--- cli/lib/fileservices.sh
+++ cli/lib/fileservices.sh
@@ -70,7 +70,7 @@ function get_btrfs_subvolume_size {
 function btrfs_recursive_delete {
 	# $1 subvolume
 	local subvols=$(btrfs subvolume list "$1" -o | awk '{print "\""$9"\""}')
-	for subvol in subvols; do
+	for subvol in "$subvols"; do
 		btrfs_recursive_delete "$THINDER_ROOT/$subvol"
 	done
 	echo btrfs subvolume delete "$1" > /dev/null
