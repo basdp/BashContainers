4f72b97c1a5bfbed1bd5f5fc362d11269891564e
Bas du Pré <me@basdupre.com>
2015-08-08 12:07:53 +0200

    loki project

--- loki/server.c
+++ loki/server.c
@@ -13,6 +13,8 @@
 #include<pthread.h> //for threading , link with lpthread
 #include <netdb.h>
 #include <netinet/in.h>
+#include <fcntl.h>
+#include <errno.h>
  
 //the thread function
 void *connection_handler(void *);
@@ -23,7 +25,7 @@ int main(int argc , char *argv[])
     struct sockaddr_in server , client;
      
     //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_desc == -1)
     {
         printf("Could not create socket");
@@ -33,10 +35,10 @@ int main(int argc , char *argv[])
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons( 80 );
      
     //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    if(bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
     {
         //print the error message
         perror("bind failed. Error");
@@ -45,7 +47,7 @@ int main(int argc , char *argv[])
     puts("bind done");
      
     //Listen
-    listen(socket_desc , 3);
+    listen(socket_desc, 3);
      
     //Accept and incoming connection
     puts("Waiting for incoming connections...");
@@ -53,9 +55,7 @@ int main(int argc , char *argv[])
 	pthread_t thread_id;
 	
     while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
-    {
-        puts("Connection accepted");
-         
+    {         
         if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &client_sock) < 0)
         {
             perror("could not create thread");
@@ -64,7 +64,6 @@ int main(int argc , char *argv[])
          
         //Now join the thread , so that we dont terminate before the thread
         //pthread_join( thread_id , NULL);
-        puts("Handler assigned");
     }
      
     if (client_sock < 0)
@@ -83,27 +82,27 @@ void *connection_handler(void *socket_desc)
 {
     //Get the socket descriptor
     int sock = *(int*)socket_desc;
-    int read_size;
-    char *message , client_message[2000];
-     
-    //Send some messages to the client
-    /*message = "Greetings! I am your connection handler\n";
-    write(sock , message , strlen(message));
-     
-    message = "Now type something and i shall repeat what you type \n";
-    write(sock , message , strlen(message));*/
+    ssize_t read_size;
+    
+    struct sockaddr_in address;
+    int c = sizeof(address);
+    getpeername(sock, (struct sockaddr*)&address, (socklen_t*)&c);
+    char ipstr[256] = { 0 };
+    inet_ntop(AF_INET, &address.sin_addr, ipstr, 256);
     
     char *host = malloc(sizeof(char) * 1024);
     strcpy(host, "");
     
-    int requestsize = sizeof(char) * 4096 * 2;
+    unsigned long requestsize = sizeof(char) * 4096 * 2;
     char *request = malloc(requestsize);
-    int requestlen = 0;
+    unsigned long requestlen = 0;
     
-	int buffersize = sizeof(char) * 4096;
+	unsigned long buffersize = sizeof(char) * 4096;
 	char *buffer = malloc(buffersize);
-	int bufferlen = 0;
+	unsigned long bufferlen = 0;
 	char lastchar = '\0';
+    
+    int forwardedForSet = 0;
 	
     //Receive a message from client
     while((read_size = recv(sock, (void*)&buffer[bufferlen], 1, 0)) > 0)
@@ -120,10 +119,21 @@ void *connection_handler(void *socket_desc)
             }
             
             if (strncmp(buffer, "Host: ", 6) == 0) {
+                //strcpy(buffer, "Host: tweakers.net");
+                //bufferlen = strlen(buffer) + 2;
                 strcpy(host, &buffer[6]);
             }
             
+            if (strncmp(buffer, "X-Forwarded-For: ", 17) == 0) {
+                forwardedForSet = 1;
+                char buffer2[4096];
+                strcpy(buffer2, buffer);
+                sprintf(buffer, "%s, %s", buffer2, ipstr);
+                bufferlen = strlen(buffer) + 2;
+            }
+            
             buffer[bufferlen - 2] = '\r';
+            buffer[bufferlen - 1] = '\n';
             
             if (requestlen + bufferlen > requestsize) {
                 request = realloc(request, requestsize * 2);
@@ -136,15 +146,6 @@ void *connection_handler(void *socket_desc)
 			bufferlen = 0;
             lastchar = '\n';
 		} else {
-            //end of string marker
-    		//client_message[read_size] = '\0';
-    		
-    		//Send the message back to client
-            //write(sock , client_message , strlen(client_message));
-    		//puts(client_message);
-    		
-    		//clear the message buffer
-    		//memset(client_message, 0, 2000);
     		lastchar = buffer[bufferlen - 1];
         }
     }
@@ -158,31 +159,30 @@ void *connection_handler(void *socket_desc)
         strcpy(portstr, sep + 1);
         *sep = '\0';
     }
-    printf("hostname: %s\nport: %i\n", host, port);
+    printf("[%s] %s:%i\n", ipstr, host, port);
     if (requestlen + 4096 > requestsize) {
         request = realloc(request, requestsize + 4096);
         requestsize += 4096;
     }
-    struct sockaddr_in address;
-    int c = sizeof(address);
-    getpeername(sock, (struct sockaddr*)&address, (socklen_t*)&c);
-    char dst[256] = { 0 };
     
-    strcat(request, "X-Forwarded-For: ");
-    strcat(request, inet_ntop(AF_INET, &address.sin_addr, dst, 256));
-    strcat(request, "\r\n");
+    if (!forwardedForSet) {
+        strcat(request, "X-Forwarded-For: ");
+        strcat(request, ipstr);
+        strcat(request, "\r\n");
+    }
+    
     strcat(request, "X-Forwarded-Host: ");
     strcat(request, host);
     strcat(request, ":");    
     strcat(request, portstr);
     strcat(request, "\r\n");
+    
+    strcat(request, "X-Forwarded-By: Thinder/Loki\r\n");
+    
     strcat(request, "\r\n");
     
-	//printf("request: \"%s\"\n", request);
-    printf("host is %s\n", host); 
-    
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
+    int serversock = socket(AF_INET, SOCK_STREAM, 0);
+    if (serversock < 0)
     {
       perror("ERROR opening socket");
       exit(1);
@@ -193,22 +193,71 @@ void *connection_handler(void *socket_desc)
       fprintf(stderr,"ERROR, no such host\n");
       exit(1);
     }
+    
+    // now set the client socket non-blocking
+    int flags;
+    if (-1 == (flags = fcntl(sock, F_GETFL, 0)))
+        flags = 0;
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+    
+    // connect to server
     struct sockaddr_in serv_addr;
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(port);
 
+    if(connect(serversock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("Error: Connect to http server failed \n");
+        exit(1);
+    }
     
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
+    write(serversock, request, strlen(request));
+    
+    if (-1 == (flags = fcntl(serversock, F_GETFL, 0)))
+        flags = 0;
+    fcntl(serversock, F_SETFL, flags | O_NONBLOCK);
+    
+    while (1) {
+        while((read_size = recv(sock, (void*)buffer, 128, 0)) > 0)
+        {
+            write(serversock, buffer, read_size);
+        }
+        if (read_size == -1) {
+            // error
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                // no error, continue
+            } else {
+                perror("client error");
+            }
+        } else if (read_size == 0) {
+            // shutdown
+            puts("client shutdown");
+            close(sock);
+            close(serversock);
+            break;
+        }
+        while((read_size = recv(serversock, (void*)buffer, 128, 0)) > 0)
+        {
+            write(sock, buffer, read_size);
+        }
+        if (read_size == -1) {
+            // error
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                // no error, continue
+            } else {
+                perror("server error");
+            }
+        } else if (read_size == 0) {
+            // shutdown
+            puts("server shutdown");
+            close(sock);
+            close(serversock);
+            break;
+        }
     }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
-    }
-         
+    puts("end thread");
+    
     return 0;
 } 
